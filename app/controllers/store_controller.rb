e6fe89383f061574118a4a66c801d383528262b1
Matthew Klein <mtt.l.kln@gmail.com>
2022-07-30 17:19:56 -0500

    owned/purchase logic

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,14 @@
 class StoreController < ApplicationController
   def index
     @store_items = HashWithIndifferentAccess.new(YAML.load(File.open("#{Rails.root}/config/store_items.yml", 'r')))[:items]
-    @user = current_user
+    @user_items = current_user.items.pluck(:name)
 
-    
+    @store_items = @store_items.each do |item| 
+      if @user_items.include? item[:name]
+        item[:owned] = true
+      else
+        item[:owned] = false
+      end
+    end
   end
 end
