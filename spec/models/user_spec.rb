b3839b47c315fe428967fcf506387455e70269f6
Matthew Klein <mtt.l.kln@gmail.com>
2022-07-30 15:19:02 -0500

    clean

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,4 @@ require "rails_helper"
 
 RSpec.describe User, :type => :model do
   it { should have_one(:cat)}
-#   before(:each) do
-#     @user = User.new
-#     @cat = Cat.new(user: @user)
-#   end
-#   # it {should have_one(:cat) } 
-#  context "it has one and only one cat" do
-#    it 'should have one cat' do
-#     u = User.new
-#     c = Cat.new(user: u)
-#     expect (u.cat.).to eq(1)
-#    end
-#  end
 end
