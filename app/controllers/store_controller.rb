94fd598f42c8e214e2fc13e66288b6cce99f9d9e
Matthew Klein <mtt.l.kln@gmail.com>
2022-07-30 17:07:05 -0500

    items table, relationship

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
   def index
     @store_items = HashWithIndifferentAccess.new(YAML.load(File.open("#{Rails.root}/config/store_items.yml", 'r')))[:items]
+    @user = current_user
+
+    
   end
 end
---
+++ db/migrate/20220730220415_add_reference_to_items.rb
@@ -0,0 +1,5 @@
+class AddReferenceToItems < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :items, :user, index: true
+  end
+end
